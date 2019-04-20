66d7cb4a181c46b77699239a8f5b456c48c640ba
miki <miki@cloudmacaca.com>
2019-04-20 18:46:24 +0800

    Demo01 Finish

---
+++ Assets/Demo01/Scripts/MoveController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private Transform transformCache;
+	[SerializeField]
+    private float moveSpeed = 1;
+    // Use this for initialization
+    void Start()
+    {
+        transformCache = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetMoveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transformCache.Translate(targetMoveDirection * moveSpeed * Time.deltaTime);
+    }
+}
